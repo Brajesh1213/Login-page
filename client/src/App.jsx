67ee4a04401cf15306ab222b78832a7143020772
brajesh mishra <brajesh.bm92@gmail.com>
2023-11-30 12:00:23 +0530

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Sinup from "./pages/Sinup";
+import Profile from "./Profile";
 
 const App = () => {
   return (
-    <div className=' text-red-400'>App</div>
-  )
-}
+    <BrowserRouter className=" text-red-400">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Sinup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
