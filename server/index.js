896a2899470687dde62fe8dda28d424ba59e659b
brajesh mishra <brajesh.bm92@gmail.com>
2023-11-30 17:03:01 +0530

    post Api

--- server/index.js
+++ server/index.js
@@ -2,9 +2,12 @@ import  express  from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRoutes from './routes/User.route.js'
+import authRouter from './routes/auth.route.js'
 
 import mongoose from 'mongoose';
-import { configDotenv } from 'dotenv';
+// import { configDotenv } from 'dotenv';
+
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("database connected");
 })
@@ -12,8 +15,9 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 const app=express();
-
+app.use(express.json());
 app.listen(3000,()=>{
     console.log("serever listen 3000");
 })
 app.use("/api/user",userRoutes);
+app.use('/api/auth',authRouter);
