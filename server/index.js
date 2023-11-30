65525033f404558af276fd03ab18dbc42f2bd765
brajesh mishra <brajesh.bm92@gmail.com>
2023-11-30 13:54:01 +0530

    database

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 import  express  from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+import { configDotenv } from 'dotenv';
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("database connected");
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app=express();
 
 app.listen(3000,()=>{
