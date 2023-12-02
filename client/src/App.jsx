fea08fd5c5ffe08b7354acfc7912cef1a9201da4
brajesh mishra <brajesh.bm92@gmail.com>
2023-12-02 17:31:06 +0530

    update header compinent make profile private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,27 @@
-// import React from 'react'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Home";
-import About from "./pages/About";
-import Signin from "./pages/Signin";
-import Sinup from "./pages/Signup";
-import Profile from "./Profile";
-import Header from "./components/Header";
+// import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup'; // Corrected Sinup to Signup
+import Profile from './Profile';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
-    <BrowserRouter className=" text-red-400">
-    <Header/>
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<Signin />} />
-        <Route path="/sign-up" element={<Sinup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-up" element={<Signup />} /> {/* Corrected Sinup to Signup */}
+
+          <Route element={<PrivateRoute />}>
+
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
