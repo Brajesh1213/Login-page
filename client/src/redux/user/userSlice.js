6611a856e50c928d3e898cd4ecb32973f63f19d5
brajesh mishra <brajesh.bm92@gmail.com>
2023-12-04 17:46:49 +0530

    signout function

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -48,6 +48,12 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    signOut:(state)=>{
+      state.currentUser=null;
+      state.loading=false;
+      state.error=false;
+
+    },
   },
 });
 
@@ -61,5 +67,6 @@ export const {
   deleteUserFailure,
   deleteUserStart,
   deleteUserSuccess,
+  signOut
 } = userSlice.actions;
 export default userSlice.reducer;
