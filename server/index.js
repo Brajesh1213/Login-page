ca1d8d4300a5426d3d6afe66aa0000b39e5a7ec3
brajesh mishra <brajesh.bm92@gmail.com>
2023-12-04 15:54:33 +0530

    create user and update user api route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import userRoutes from './routes/User.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 
 import mongoose from 'mongoose';
 // import { configDotenv } from 'dotenv';
@@ -16,11 +17,15 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
+
 app.listen(3000,()=>{
     console.log("serever listen 3000");
 })
+
 app.use("/api/user",userRoutes);
 app.use('/api/auth',authRouter);
+
 app.use((err,req,res,next)=>{
 const statusCode=err.statusCode || 500;
 const message = err.message||'Internal Server Error';
