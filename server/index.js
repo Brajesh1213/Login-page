92f9c47145ea6080d7a14a687080ed6ead61138d
Brajesh kumar Mishra <108130727+Brajesh1213@users.noreply.github.com>
2023-12-29 11:03:27 +0530

    Update index.js
    direct

--- server/index.js
+++ server/index.js
@@ -1,44 +1,46 @@
-import  express  from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import userRoutes from './routes/User.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
 
 dotenv.config();
-import userRoutes from './routes/User.route.js'
-import authRouter from './routes/auth.route.js'
-// import userDetail from './routes/userDetail.js'
-import cookieParser from 'cookie-parser';
-
-
-import mongoose from 'mongoose';
-// import { configDotenv } from 'dotenv';
-
-
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log("database connected");
-})
-.catch((err)=>{
-    console.log(err);
-})
-const app=express();
-app.use(express.json());
-app.use(cookieParser());
-
-app.listen(3000,()=>{
-    console.log("serever listen 3000");
-})
-
-app.use("/api/user",userRoutes);
-app.use('/api/auth',authRouter);
-// app.use('/dashboard',userDetail);
-
-
-app.use((err,req,res,next)=>{
-const statusCode=err.statusCode || 500;
-const message = err.message||'Internal Server Error';
-return res.status(statusCode).json({
-    success:false,
-    message,
-    statusCode,
-
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Database connected");
+}).catch((err) => {
+  console.error(err);
 });
 
-})
+const __dirname = path.resolve();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(cookieParser());
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+
+app.use("/api/user", userRoutes);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
